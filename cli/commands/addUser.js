e7d9f35eb6e65ef99094143f46b3c6ebea00bace
MikeWaser <mik3waser@gmail.com>
2024-12-20 15:54:32 +0100

    getEventDetails update

--- cli/commands/addUser.js
+++ cli/commands/addUser.js
@@ -1,3 +1,4 @@
+//1
 import axios from "axios";
 import { URL } from "../config.js";
 
@@ -22,3 +23,33 @@ export function addUser(program) {
         .description("Register a user to an event")
         .action(addUserToEvent);
 }
+
+/* 
+import axios from "axios"; // För HTTP-förfrågningar
+import { URL } from "../config.js"; // Backend-URL från konfigurationen
+
+// Lägg till en användare till ett event
+async function addUserToEvent(eventId, username) {
+    try {
+        // Skicka en PUT-förfrågan till backend för att registrera användaren
+        const response = await axios.put(`${URL}/events/${eventId}/register`, { username });
+
+        // Bekräftelsemeddelande
+        console.log(`Användaren ${username} registrerades till eventet "${response.data.name}".`);
+    } catch (err) {
+        // Felhantering: Om backend returnerar ett fel eller nätverket misslyckas
+        if (err.response) {
+            console.error("Fel vid registrering:", err.response.data.message || err.response.data);
+        } else {
+            console.error("Ett nätverksfel inträffade:", err.message);
+        }
+    }
+}
+
+// Registrerar kommandot "register" för CLI
+export function addUser(program) {
+    program
+        .command("register <eventId> <username>")
+        .description("Registrera en användare till ett event")
+        .action(addUserToEvent);
+} */
