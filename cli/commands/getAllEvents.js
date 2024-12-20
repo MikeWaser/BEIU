93f23a4b7273b5b1c5e3e2a155e5c146746be3c5
MikeWaser <mik3waser@gmail.com>
2024-12-20 14:06:11 +0100

    getAllEvents update

--- cli/commands/getAllEvents.js
+++ cli/commands/getAllEvents.js
@@ -1,34 +1,4 @@
-/* import axios from "axios";
-import { URL } from "../config.js";
-
-// Hämtar och listar alla offentliga events
-async function listPublicEvents() {
-    try {
-        // Skicka en förfrågan till api:et för att hämta offentliga events
-        const response = await axios.get(`${URL}/events/public`);
-        console.log("Public Events:");
-        response.data.forEach((event, index) => {
-            console.log(`${index + 1}. ${event.name} - ${event.location} on ${event.date}`);
-        });
-    } catch (error) {
-       // Kollar om det finns något fel i svaret från api:et och skickar tillbaka felmeddelanden isf
-        if (error.response) {
-            console.error("Error fetching public events:", error.response.data.message || error.response.data);
-        } else {
-            console.error("Network error or server not reachable:", error.message);
-        }
-    }
-}
-
-// Funktion för att registrera kommandot "list" med programmet för att lista alla offentliga events
-export function getAllEvents (program){
-program
-        .command("list")
-        .description("List all public events")
-        .action(listPublicEvents);
-    } */
-
-        import axios from "axios";
+import axios from "axios";
 import { URL } from "../config.js";
 
 async function listEvents() {
